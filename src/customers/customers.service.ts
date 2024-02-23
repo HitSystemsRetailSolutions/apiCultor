3e1f3893d62bfb56857e2a6bdb4ad1fcd2114eab
Ana <amartinez@hitsystems.es>
2024-02-23 11:43:05 +0100

    Sincronizando facturas

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -19,34 +19,6 @@ export class customersService {
     private sql: runSqlService,
   ) {}
 
-  //Obtener Id de Company
-  async getCompaniesId() {
-    let token = await this.token.getToken();
-    let pTermCode = "0D";
-    let res = await axios.get(
-      `${process.env.baseURL}/v2.0/${process.env.tenant}/production/api/v2.0/companies`,
-      {
-        headers: {
-          Authorization: 'Bearer ' + token,
-          'Content-Type': 'application/json',
-        },
-      },
-    );
-
-    if (!res.data) throw new Error('Failed to obtain access token');
-    let companies = res.data.value;
-    if (companies.length === 0) 
-    {
-        console.log('NO HAY COMPANIES');
-    }
-    else
-    {
-      for (let i=0;i<companies.length;i++)
-      {
-        console.log(companies[i]);
-      }
-    }
-  }
 
   //Obtener Id del modo de pago
   async getPaymentTermId(pTermCode, companyID) {
@@ -93,9 +65,6 @@ export class customersService {
   }
 
   async syncCustomers(companyID: string, database: string) {
-    //En todo el documento process.env.database y process.env.companyID han sido sustituidos por database y companyID respectivamente
-    console.log(companyID);
-    console.log(database);
     let token = await this.token.getToken();
     let customerId = '';
 
@@ -160,21 +129,6 @@ export class customersService {
               postalCode: x.CP,
               phoneNumber: x.Tel,
               email: x.eMail,              
-              //paymentTermsId: payTermId,
-              //paymentMethodId: 'ebb54901-3110-ee11-8f6e-6045bd978b14', //CAJA
-              //addressLine2: '',
-              //state: '',
-              //country: '',
-              //website: '',
-              //salespersonCode: '',
-              //balanceDue: 0,
-              //creditLimit: 0,
-              //taxLiable: false,
-              //taxRegistrationNumber: '', (NIF)
-              //currencyId: '00000000-0000-0000-0000-000000000000',
-              //shipmentMethodId: '00000000-0000-0000-0000-000000000000',
-              //blocked: '_x0020_',
-              //businessGroup: 'UE',
             },
             {
               headers: {
@@ -189,19 +143,6 @@ export class customersService {
 
         if (!newCustomers.data)
           return new Error('Failed Post Customer ' + x.Codi);
-
-        //console.log(
-        //  'Synchronizing customers... -> ' +
-//            i +
-//            '/' +
-//            customers.recordset.length,
-//          ' --- ',
-//          ((i / customers.recordset.length) * 100).toFixed(2) + '%',
-//          ' | Time left: ' +
-//            ((customers.recordset.length - i) * (0.5 / 60)).toFixed(2) +
-//            ' minutes',
-//        );
-
         customerId = newCustomers.data.id;
 
       } else { //YA EXISTE EL CLIENTE EN BC, LO TENEMOS QUE ACTUALIZAR
@@ -221,9 +162,6 @@ export class customersService {
               postalCode: x.CP,
               phoneNumber: x.Tel,
               email: x.eMail,              
-              //businessGroup: 'UE',
-              //paymentTermsId: payTermId,
-              //paymentMethodId: 'ebb54901-3110-ee11-8f6e-6045bd978b14', //CAJA
             },
             {
               headers: {
@@ -238,23 +176,12 @@ export class customersService {
           });
         if (!newCustomers.data)
           return new Error('Failed update customer');
-//        console.log(
-//          'Synchronizing customers... -> ' +
-//            i +
-//            '/' +
-//            customers.recordset.length,
-//          ' --- ',
-//          ((i / customers.recordset.length) * 100).toFixed(2) + '%',
-//         ' | Time left: ' +
-//            ((customers.recordset.length - i) * (0.5 / 60)).toFixed(2) +
-//            ' minutes',
-//        );
       }
     }
     return true;
   }
 
-  async getCustomerFromAPI(codiHIT) {
+  async getCustomerFromAPI(companyID, database, codiHIT) {
     let customerId = '';
 
     // Get the authentication token
@@ -263,7 +190,7 @@ export class customersService {
     // Get Customer from API
     let res = await axios
       .get(
-        `${process.env.baseURL}/v2.0/${process.env.tenant}/production/api/v2.0/companies(${process.env.companyID})/customers?$filter=number eq '${codiHIT}'`,
+        `${process.env.baseURL}/v2.0/${process.env.tenant}/production/api/v2.0/companies(${companyID})/customers?$filter=number eq '${codiHIT}'`,
         {
           headers: {
             Authorization: 'Bearer ' + token,
@@ -272,12 +199,64 @@ export class customersService {
         },
       )
       .catch((error) => {
-        throw new Error('Failed to obtain access token');
+        throw new Error('Failed to obtain customer');
       });
 
-    if (!res.data) throw new Error('Failed to obtain access token');
+    if (!res.data) throw new Error('Failed to obtain customer');
 
-    if (res.data.value.length === 0) {
+    if (res.data.value.length === 0) { //NO ESTÁ EL CLIENTE EN BC, LO TENEMOS QUE TRASPASAR
+
+      console.log("CLIENTE NUEVO ---------------------");
+      let customers;
+      let taxId = await this.getTaxAreaId('UE', companyID);
+         
+      try{
+        customers = await this.sql.runSql(
+          `SELECT cast(c.Codi as nvarchar) Codi, upper(c.Nom) Nom, c.Adresa, c.Ciutat, c.CP, cc1.valor Tel, cc2.valor eMail from clients c left join constantsClient cc1 on c.codi= cc1.codi and cc1.variable='Tel' join constantsClient cc2 on c.codi= cc2.codi and cc2.variable='eMail' where c.codi=${codiHIT} order by c.codi`,
+          database,
+        );
+      } catch (error){ //Comprovacion de errores y envios a mqtt
+        client.publish('/Hit/Serveis/Apicultor/Log', 'Customers. No existe la database');
+        console.log('Customers. No existe la database')
+        return false;
+      }
+     
+      if(customers.recordset.length == 0){ //Comprovacion de errores y envios a mqtt
+        client.publish('/Hit/Serveis/Apicultor/Log', 'Customers. No hay registros');
+        console.log('Customers. No hay registros')
+        return false;
+      }
+
+      let x = customers.recordset[0];
+      let newCustomers = await axios
+        .post(
+          `${process.env.baseURL}/v2.0/${process.env.tenant}/production/api/v2.0/companies(${companyID})/customers`,
+          {
+            number: x.Codi,
+            displayName: x.Nom,
+            type: 'Company',
+            taxAreaId: taxId,
+            currencyCode: 'EUR',
+            addressLine1: x.Adresa,
+            city: x.Ciutat,              
+            postalCode: x.CP,
+            phoneNumber: x.Tel,
+            email: x.eMail,              
+          },
+          {
+            headers: {
+              Authorization: 'Bearer ' + token,
+              'Content-Type': 'application/json',
+            },
+          },
+        )
+        .catch((error) => {
+          throw new Error('Failed Post Customer ' + x.Codi);
+        });
+
+      if (!newCustomers.data)
+        return new Error('Failed Post Customer ' + x.Codi);
+      customerId = newCustomers.data.id;
     } else {
       customerId = res.data.value[0].id;
     }
