3e2b3a679bf474d1017bd0d439bc12919c7ae9fc
Ana <amartinez@hitsystems.es>
2024-02-23 11:44:47 +0100

    Sincroniza facturas

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -19,9 +19,6 @@ export class itemsService {
   ) {}
 
   async syncItems(companyID: string, database: string) {
-    //En todo el documento process.env.database y process.env.companyID han sido sustituidos por database y companyID respectivamente
-    console.log(companyID)
-    console.log(database)
     let token = await this.token.getToken();
     let itemId = '';
 
@@ -33,19 +30,19 @@ export class itemsService {
       );
     } catch (error){ //Comprovacion de errores y envios a mqtt
       client.publish('/Hit/Serveis/Apicultor/Log', 'No existe la database');
-      console.log('No existe la database')
+      console.log('Items. No existe la database')
       return false;
     }
     
     if(items.recordset.length == 0){ //Comprovacion de errores y envios a mqtt
       client.publish('/Hit/Serveis/Apicultor/Log', 'No hay registros');
-      console.log('No hay registros')
+      console.log('Items. No hay registros')
       return false;
     }
     
     for (let i = 0; i < items.recordset.length; i++) {
       let x = items.recordset[i];
-      console.log(x.Nom);
+
       let baseUnitOfMeasure = "UDS";
       //Unidad de medida (obligatorio)
       if (x.EsSumable === 0){
@@ -82,10 +79,7 @@ export class itemsService {
               displayName: x.Nom,                        
               generalProductPostingGroupCode: 'IVA'+x.Iva,
               unitPrice: x.Preu,
-              //priceIncludesTax: true,
-              //itemCategoryId: categoryId,
               baseUnitOfMeasureCode: baseUnitOfMeasure,
-              //inventoryPostingGroupCode: '001',
             },
             {
               headers: {
@@ -100,17 +94,6 @@ export class itemsService {
 
         if (!newItems.data)
           return new Error('Failed post item');
-//        console.log(
-//          'Synchronizing items... -> ' +
-//            i +
-//            '/' +
-//            items.recordset.length,
-//          ' --- ',
-//          ((i / items.recordset.length) * 100).toFixed(2) + '%',
-//          ' | Time left: ' +
-//            ((items.recordset.length - i) * (0.5 / 60)).toFixed(2) +
-//            ' minutes',
-//        );
         itemId = newItems.data.id;
       } else {
         let z = res.data.value[0]['@odata.etag'];
@@ -125,9 +108,7 @@ export class itemsService {
               generalProductPostingGroupCode: 'IVA'+x.Iva,
               unitPrice: x.Preu,
               priceIncludesTax: true,
-              //itemCategoryId: categoryId,
               baseUnitOfMeasureCode: baseUnitOfMeasure,
-              //inventoryPostingGroupCode: '001',
             },
             {
               headers: {
@@ -142,20 +123,83 @@ export class itemsService {
           });
         if (!newItems.data)
           return new Error('Failed to update item');
-//        console.log(
-//          'Synchronizing items... -> ' +
-//            i +
-//            '/' +
-//            items.recordset.length,
-//          ' --- ',
-//          ((i / items.recordset.length) * 100).toFixed(2) + '%',
-//          ' | Time left: ' +
-//            ((items.recordset.length - i) * (0.5 / 60)).toFixed(2) +
-//            ' minutes',
-//        );
       }
     }
     return true;
+  }
+
+  async getItemFromAPI(companyID, database, codiHIT) {
+    let itemId = '';
+
+    // Get the authentication token
+    let token = await this.token.getToken();
+    let items;
+    let sqlQ1= 'SELECT a.Codi, a.Nom, a.Preu/(1+(t.Iva/100)) PreuSinIva, a.Preu, left(a.Familia, 20) Familia, a.EsSumable, t.Iva FROM (select codi, nom, preu, familia, esSumable, tipoIva from Articles union all select codi, nom, preu, familia, esSumable, tipoIva from articles_Zombis) a left join tipusIva2012 t on a.Tipoiva=t.Tipus where a.codi='+ codiHIT;
+
+    try {
+      items = await this.sql.runSql(
+        sqlQ1,
+        database,
+      );
+    } catch (error){
+      console.log(error)
+    }
+
+    let baseUnitOfMeasure = "UDS";
+      //Unidad de medida (obligatorio)
+      if (items.recordset[0].EsSumable === 0){
+         baseUnitOfMeasure = "KG"; //A peso
+      }
+      else{
+         baseUnitOfMeasure = "UDS"; //Por unidades
+      }
+
+    let url =  `${process.env.baseURL}/v2.0/${process.env.tenant}/production/api/v2.0/companies(${companyID})/items?$filter=number eq 'CODI-${codiHIT}'`;
+
+    // Get Item from API
+    let res = await axios
+      .get(
+        url,
+        {
+          headers: {
+            Authorization: 'Bearer ' + token,
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+      .catch((error) => {
+        throw new Error('Failed to obtain item');
+      });
+       
+    if (!res.data) throw new Error('Failed to obtain item');
+
+    if (res.data.value.length === 0) {
+      let newItems = await axios
+          .post(
+            `${process.env.baseURL}/v2.0/${process.env.tenant}/production/api/v2.0/companies(${companyID})/items`,
+            {
+              number: 'CODI-' + codiHIT,
+              displayName: items.recordset[0].Nom,                        
+              generalProductPostingGroupCode: 'IVA'+items.recordset[0].Iva,
+              unitPrice: items.recordset[0].Preu,
+              baseUnitOfMeasureCode: baseUnitOfMeasure,
+            },
+            {
+              headers: {
+                Authorization: 'Bearer ' + token,
+                'Content-Type': 'application/json',
+              },
+            },
+          )
+          .catch((error) => {
+            throw new Error('Failed post item ' + items.recordset[0].Nom);
+          });
+        itemId = newItems.data.id;  
+    } else {
+        itemId = res.data.value[0].id;
+    }
+
+    return itemId;
   }
 
 }
--- src/sales/salesFacturas.service.ts
+++ src/sales/salesFacturas.service.ts
@@ -3,117 +3,17 @@ import { getTokenService } from '../conection/getToken.service';
 import { runSqlService } from 'src/conection/sqlConection.service';
 import axios from 'axios';
 import { response } from 'express';
+import { customersService } from 'src/customers/customers.service';
+import { itemsService } from 'src/items/items.service';
 @Injectable()
 
 export class salesFacturasService {
   constructor(
     private token: getTokenService,
     private sql: runSqlService,
+    private customers: customersService,
+    private items: itemsService,
   ) {}
-
-  async getCustomerFromAPI(companyID, codiHIT) {
-    let customerId = '';
-
-    // Get the authentication token
-    let token = await this.token.getToken();
-
-    // Get Customer from API
-    let res = await axios
-      .get(
-        `${process.env.baseURL}/v2.0/${process.env.tenant}/production/api/v2.0/companies(${companyID})/customers?$filter=number eq '${codiHIT}'`,
-        {
-          headers: {
-            Authorization: 'Bearer ' + token,
-            'Content-Type': 'application/json',
-          },
-        },
-      )
-      .catch((error) => {
-        throw new Error('Failed to obtain customer');
-      });
-
-    if (!res.data) throw new Error('Failed to obtain customer');
-
-    if (res.data.value.length === 0) {
-    } else {
-      customerId = res.data.value[0].id;
-    }
-    return customerId;
-  }
-
-async getItemFromAPI(companyID, database, codiHIT) {
-    let itemId = '';
-
-    // Get the authentication token
-    let token = await this.token.getToken();
-    let items;
-    let sqlQ1= 'SELECT a.Codi, a.Nom, a.Preu/(1+(t.Iva/100)) PreuSinIva, a.Preu, left(a.Familia, 20) Familia, a.EsSumable, t.Iva FROM (select codi, nom, preu, familia, esSumable, tipoIva from Articles union all select codi, nom, preu, familia, esSumable, tipoIva from articles_Zombis) a left join tipusIva2012 t on a.Tipoiva=t.Tipus where a.codi='+ codiHIT +' order by a.codi';
-    console.log(sqlQ1)
-    try {
-      items = await this.sql.runSql(
-        sqlQ1,
-        database,
-      );
-    } catch (error){
-      console.log(error)
-    }
-    let baseUnitOfMeasure = "UDS";
-      //Unidad de medida (obligatorio)
-      if (items.recordset[0].EsSumable === 0){
-         baseUnitOfMeasure = "KG"; //A peso
-      }
-      else{
-         baseUnitOfMeasure = "UDS"; //Por unidades
-      }
-    let url =  `${process.env.baseURL}/v2.0/${process.env.tenant}/production/api/v2.0/companies(${companyID})/items?$filter=number eq 'CODI-${codiHIT}'`;
-    //console.log(url);
-    // Get Item from API
-    let res = await axios
-      .get(
-        url,
-        {
-          headers: {
-            Authorization: 'Bearer ' + token,
-            'Content-Type': 'application/json',
-          },
-        },
-      )
-      .catch((error) => {
-        throw new Error('Failed to obtain item');
-      });
-       
-    if (!res.data) throw new Error('Failed to obtain item');
-    //console.log(res.data.value[0])
-    if (res.data.value.length === 0) {
-      let newItems = await axios
-          .post(
-            `${process.env.baseURL}/v2.0/${process.env.tenant}/production/api/v2.0/companies(${companyID})/items`,
-            {
-              number: 'CODI-' + codiHIT,
-              displayName: items.recordset[0].Nom,                        
-              generalProductPostingGroupCode: 'IVA'+items.recordset[0].Iva,
-              unitPrice: items.recordset[0].Preu,
-              //priceIncludesTax: true,
-              //itemCategoryId: categoryId,
-              baseUnitOfMeasureCode: baseUnitOfMeasure,
-              //inventoryPostingGroupCode: '001',
-            },
-            {
-              headers: {
-                Authorization: 'Bearer ' + token,
-                'Content-Type': 'application/json',
-              },
-            },
-          )
-          .catch((error) => {
-            throw new Error('Failed post item ' + items.recordset[0].Nom);
-          });
-    } else {
-        itemId = res.data.value[0].id;
-    }
-    //console.log('itemId:', itemId)
-    return itemId;
-  }
 
   async getSaleFromAPI(companyID, docNumber) {
     // Get the authentication token
@@ -165,9 +65,13 @@ async getItemFromAPI(companyID, database, codiHIT) {
     } else{
       num = serieFactura + numFactura;
     }
-    let customerId = await this.getCustomerFromAPI(companyID, x.ClientCodi);
-    let idSaleHit = x.Id;
-    console.log("---------------------------------------" + num + "-----------------------------------------");
+
+    console.log("-------------------SINCRONIZANDO FACTURA NÚMERO " + num + " -----------------------");
+    let customerId = await this.customers.getCustomerFromAPI(companyID, database, x.ClientCodi);
+    //console.log("CLIENTE BC: " + customerId);
+
+    let idSaleHit = x.IdFactura;
+
     let url = `${process.env.baseURL}/v2.0/${process.env.tenant}/production/api/v2.0/companies(${companyID})/salesInvoices?$filter=externalDocumentNumber eq '${num}'`
     //console.log(url)
     let res = await axios
@@ -215,18 +119,17 @@ async getItemFromAPI(companyID, database, codiHIT) {
         else{
           facturaId_BC = newFacturas.data.id;
           await this.synchronizeSalesFacturasLines(companyID, database, tabFacturacioDATA, x.IdFactura, facturaId_BC).catch(console.error);
-          //console.log("------------------------------------------------------" + x.Serie + x.NumFactura + "-------------------------------");
+
           let resSale = await this.getSaleFromAPI(companyID, num);
           if (!resSale.data) throw new Error('Failed to obtain ticket BS');
           if (resSale.data.value.length != 0) {          
-            //console.log(resSale);
             sqlQ = "update [BC_SyncSales_2024] set ";
             sqlQ = sqlQ + "BC_IdSale='" + resSale.data.value[0].id + "', ";
             sqlQ = sqlQ + "BC_Number='" + resSale.data.value[0].number + "', ";
             sqlQ = sqlQ + "BC_PostingDate='" + resSale.data.value[0].postingDate + "', ";
             sqlQ = sqlQ + "BC_CustomerId= '" + resSale.data.value[0].customerId + "', ";
             sqlQ = sqlQ + "BC_totalAmountIncludingTax= '" + resSale.data.value[0].totalAmountIncludingTax + "' ";
-            sqlQ = sqlQ + "where id ='" + idSaleHit + "'";
+            sqlQ = sqlQ + "where HIT_IdFactura ='" + idSaleHit + "'";
             let updBC = await this.sql.runSql(
               sqlQ,
               database,
@@ -238,35 +141,13 @@ async getItemFromAPI(companyID, database, codiHIT) {
         console.log('Ya existe la factura');
         facturaId_BC = res.data.value[0]['id'];
         await this.synchronizeSalesFacturasLines(companyID, database, tabFacturacioDATA, x.IdFactura, facturaId_BC).catch(console.error);
-        /*
-        console.log(res.data);
-        facturaId_BC = res.data.value[0]['id'];
-        await this.synchronizeSalesFacturasLines(tabFacturacioDATA, x.IdFactura, facturaId_BC ).catch(console.error);
-
-        console.log(res.data);
-        sqlQ = "update [BC_SyncSales_2024] set ";
-        sqlQ = sqlQ + "BC_IdSale='" + res.data.value[0].id + "', ";
-        sqlQ = sqlQ + "BC_Number='" + res.data.value[0].number + "', ";
-        sqlQ = sqlQ + "BC_PostingDate='" + res.data.value[0].postingDate + "', ";
-        sqlQ = sqlQ + "BC_CustomerId= '" + res.data.value[0].customerId + "', ";
-        sqlQ = sqlQ + "BC_totalAmountIncludingTax= '" + res.data.value[0].totalAmountIncludingTax + "' ";
-        sqlQ = sqlQ + "where id ='" + idSaleHit + "'";
-        let updBC = await this.sql.runSql(
-          sqlQ,
-          'fac_hitrs',
-        );
-        */
       }
     return true;
   }
 
   async synchronizeSalesFacturasLines(companyID, database, tabFacturacioDATA, Hit_IdFactura, BC_facturaId) {
     let token = await this.token.getToken();
-//console.log("HIT_IdFactura: " + Hit_IdFactura + "-----------------------------");
-//console.log("BC_facturaId: " + BC_facturaId + "-----------------------------");
-    console.log('-----------', tabFacturacioDATA, '__________________')
-    console.log('-----------', Hit_IdFactura, '--------------')
-    console.log('-----------', BC_facturaId, '--------------')
+
     let sqlQ;
     sqlQ = "select sum(f.Servit) Quantitat, sum(f.preu + f.preu*(iva/100))/sum(f.servit) UnitPrice, CAST(f.Producte as varchar) Plu ";
     sqlQ = sqlQ + "From " + tabFacturacioDATA + " f ";
@@ -276,14 +157,14 @@ async getItemFromAPI(companyID, database, codiHIT) {
       sqlQ,
       database,
     );
-    console.log(sqlQ);
+    
     for (let i = 0; i < facturasLines.recordset.length; i++) {
       let x = facturasLines.recordset[i];
-      console.log("-------------------------PPRODUCTO " + x.Plu);
-      let itemId = await this.getItemFromAPI(companyID, database, x.Plu);
+      console.log("PPRODUCTO " + x.Plu);
+      let itemId = await this.items.getItemFromAPI(companyID, database, x.Plu);
       
       let url2 = `${process.env.baseURL}/v2.0/${process.env.tenant}/production/api/v2.0/companies(${companyID})/salesInvoices(${BC_facturaId})/salesInvoiceLines?$filter=lineObjectNumber eq 'CODI-${x.Plu}'`;
-     //console.log('-------', url2);
+
       //BUSCAMOS LA LINEA DE FACTURA
       let res = await axios
         .get(
@@ -305,7 +186,7 @@ async getItemFromAPI(companyID, database, codiHIT) {
       }
       //NO ESTÁ, LA AÑADIMOS
       let url =  `${process.env.baseURL}/v2.0/${process.env.tenant}/production/api/v2.0/companies(${companyID})/salesInvoices(${BC_facturaId})/salesInvoiceLines`;
-      //console.log(url);
+
       if (res.data.value.length === 0) {
         let newFacturas = await axios
           .post(
@@ -363,7 +244,6 @@ async getItemFromAPI(companyID, database, codiHIT) {
     }
     return true;    
   }
-
 
   async getPDFSale(companyID, nFactura) {
     let token = await this.token.getToken();
