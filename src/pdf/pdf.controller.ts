1fb3f2b647a581eac0664251487903dedfa61e38
Jonatan <jonatanfunesaguera@gmail.com>
2024-02-29 12:49:54 +0100

    subirPdf y getPdf cambiados

--- src/pdf/pdf.controller.ts
+++ src/pdf/pdf.controller.ts
@@ -3,22 +3,23 @@ import { Response } from 'express';
 import { PdfService } from './pdf.service';
 
 interface SubirPdfRequest {
-  nombre: string;
+  id: string; // Id de BC
   archivo: string; // Base64
+  database: string // Base de datos
 }
 
 @Controller()
 export class PdfController {
   constructor(private readonly pdfService: PdfService) {}
 
-  @Get('pdf/:pdfName')
-  async getPdf(@Param('pdfName') pdfName: string, @Res() res: Response) {
+  @Get('/pdf/:database/:id')
+  async getPdf(@Param('database') database: string, @Param('id') id: string, @Res() res: Response) {
     try {
-      const result = await this.pdfService.getPdf(pdfName);
+      const result = await this.pdfService.getPdf(database, id);
       if (result.success) {
         // Configura los headers de la respuesta para indicar que es un archivo PDF
         res.setHeader('Content-Type', 'application/pdf');
-        res.setHeader('Content-Disposition', `attachment; filename="${pdfName}.pdf"`);
+        res.setHeader('Content-Disposition', `attachment; filename="Factura.pdf"`); //Factura es el nombre
 
         // Envía el contenido del PDF como respuesta
         res.send(result.pdfData);
@@ -31,18 +32,20 @@ export class PdfController {
     }
   }
 
-  @Post('subirPDF')
+  @Post('/pdf/subirPDF')
   async subirPdf(@Body() body: SubirPdfRequest, @Res() res: Response) {
-    const { nombre, archivo } = body;
+    const { id, archivo, database } = body;
 
     if (!archivo) {
       return res.status(400).json({ msg: "No se proporcionó un archivo Base64" });
-    } else if (!nombre){
-      return res.status(400).json({ msg: "No se proporcionó un nombre" });
+    } else if (!database){
+      return res.status(400).json({ msg: "No se proporcionó una base de datos" });
+    } else if (!id){
+      return res.status(400).json({ msg: "No se proporcionó un ID" });
     }
     
     try {
-      const result = await this.pdfService.subirPdf(nombre, archivo);
+      const result = await this.pdfService.subirPdf(id, archivo, database);
       return res.status(200).json(result);
     } catch (error) {
       console.error('Error al subir el PDF:', error);
--- src/pdf/pdf.service.ts
+++ src/pdf/pdf.service.ts
@@ -1,35 +1,71 @@
 import { Injectable } from '@nestjs/common';
 import { getTokenService } from '../conection/getToken.service';
 import { runSqlService } from 'src/conection/sqlConection.service';
+import * as nodemailer from 'nodemailer';
+import * as mailgunTransport from 'nodemailer-mailgun-transport';
 
 @Injectable()
-export class PdfService  {
+export class PdfService {
   constructor(
     private token: getTokenService,
     private sql: runSqlService,
   ) {}
 
-  async obtenerFragmentosDeArchivo(nombreArchivo: string) {
+  async enviarCorreoConPdf(pdfData) {
+    try {
+      // Configuración del transporte SMTP para Gmail
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'jfunesa@ies-sabadell.cat', // Reemplaza con tu dirección de correo de Gmail
+          pass: process.env.miPassword // Reemplaza con tu contraseña de Gmail
+        }
+      });
+  
+      // Opciones del correo electrónico
+      const mailOptions = {
+        from: 'jonatanfunesaguera@gmail.com', // Debe ser la misma que la dirección de correo de Gmail utilizada en 'auth.user'
+        to: 'jfunesa@ies-sabadell.cat',
+        subject: 'PDF adjunto',
+        text: 'Adjunto encontrarás el PDF solicitado.',
+        attachments: [
+          {
+            filename: 'archivo.pdf', //archivo es el nombre
+            content: pdfData,
+            encoding: 'base64'
+          }
+        ]
+      };
+  
+      // Envío del correo electrónico
+      const info = await transporter.sendMail(mailOptions);
+      console.log('Correo enviado:', info);
+  
+      return { success: true };
+    } catch (error) {
+      console.error('Error al enviar el correo electrónico:', error);
+      return { success: false, message: 'Error al enviar el correo electrónico' };
+    }
+  }
+
+  async obtenerFragmentosDeArchivo(id: string, database: string) {
     // Realiza una consulta SQL para recuperar los fragmentos asociados con el archivo
     const sql = `
-      SELECT archivo FROM archivo WHERE nombre = '${nombreArchivo}' ORDER BY descripcion;
+      SELECT BC_PDF FROM BC_SyncSales_2024 WHERE BC_IdSale = '${id}' ORDER BY HIT_DataFactura;
     `;
     let pdf;
     try{
-      pdf =  await this.sql.runSql(
-        sql,
-        'Fac_Tena',
-    );
+      pdf =  await this.sql.runSql(sql,database,);
     } catch{
         console.log("Error")
     }
-    return pdf.recordset.map(row => Buffer.from(row.archivo, 'hex'));
+    return pdf.recordset.map(row => Buffer.from(row.BC_PDF, 'hex'));
   }
 
-  async getPdf(pdfName: string) {
+  async getPdf(database: string, id: string) {
     try {
       // Obtén todos los fragmentos asociados con el archivo
-      const fragmentos = await this.obtenerFragmentosDeArchivo(pdfName);
+      const fragmentos = await this.obtenerFragmentosDeArchivo(id, database);
 
       // Verifica si el archivo existe
       if (!fragmentos || fragmentos.length === 0) {
@@ -40,20 +76,20 @@ export class PdfService  {
       // Concatena todos los fragmentos para reconstruir el archivo original
       const archivoCompleto = Buffer.concat(fragmentos);
 
-      // No envíes la respuesta aquí, simplemente devuelve el archivo completo.
-      return { success: true, pdfData: archivoCompleto };
+      // Envía el PDF por correo electrónico
+      await this.enviarCorreoConPdf(archivoCompleto);
 
+      // Devuelve el PDF
+      return { success: true, pdfData: archivoCompleto };
     } catch (error) {
       console.error("Error al descargar el archivo:", error);
       return { success: false, message: "Error al descargar el archivo" };
     }
   }
 
-  async subirPdf(nombre: string, archivoBase64: string) {
+  async subirPdf(id: string, archivoBase64: string, database: string) {
     // Convierte el Base64 a Buffer
     const bufferArchivo = Buffer.from(archivoBase64, 'base64');
-
-    // Divide el Buffer en fragmentos más pequeños (por ejemplo, de 1 KB)
     const chunks = [];
     const chunkSize = bufferArchivo.length; // Tamaño de cada fragmento en bytes
     for (let i = 0; i < bufferArchivo.length; i += chunkSize) {
@@ -62,19 +98,20 @@ export class PdfService  {
     }
 
     try {
-      // Inserta los fragmentos en filas separadas
       for (let i = 0; i < chunks.length; i++) {
         const descripcion = `part ${i}`;
+        /*
         const sql = `
           INSERT INTO archivo (id, nombre, extension, descripcion, archivo)
           VALUES (newid(), '${nombre}', 'PDF', '${descripcion}', 0x${chunks[i]})
         `;
+        */
+        const sql = `
+          UPDATE BC_SyncSales_2024 SET BC_PDF=0x${chunks[i]} WHERE BC_IdSale='${id}'
+        `;
         let pdf;
         try{
-          pdf =  await this.sql.runSql(
-            sql,
-            'Fac_Tena',
-        );
+          pdf =  await this.sql.runSql(sql,database,);
         } catch{
             console.log("Error")
         }
