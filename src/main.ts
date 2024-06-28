48ca48ac1388330c3e1c0d93de0da075986b46f6
Jonatan Funes <jonatanfunesaguera@gmail.com>
2024-06-28 15:31:41 +0200

    no

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ client.on('connect', function () {
   console.log('Conectado al broker MQTT');
 
   // Suscribirse a un tema
-  let tema = '/Testing/Hit/Serveis/Apicultor';
+  let tema = '/Hit/Serveis/Apicultor';
   client.subscribe(tema, function (err) {
     if (err) {
       console.error('Error al suscribirse al tema', err);
