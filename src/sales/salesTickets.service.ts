318c05af2a9c3386e46893e03d24aaa72bec19eb
Jonatan Funes <jonatanfunesaguera@gmail.com>
2024-07-01 17:42:27 +0200

    data

--- src/sales/salesTickets.service.ts
+++ src/sales/salesTickets.service.ts
@@ -381,7 +381,7 @@ export class salesTicketsService {
             ' minutes',
           );
 
-          let sqlUpdate = `update records set timestamp='${x.tmstStr}' where concepte='BC_SalesTickets_` + botiga + `'`;
+          let sqlUpdate = `update records set timestamp='${x.Data}' where concepte='BC_SalesTickets_` + botiga + `'`;
           await this.sql.runSql(
             sqlUpdate,
             database,
