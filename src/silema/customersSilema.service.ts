b5b9c642f870116dc8aac1b29646b0cdecf9ef04
Jonatan Funes <jonatanfunesaguera@gmail.com>
2025-01-30 16:32:12 +0100

    Si ya existe un cliente con ese NIF no se insertara nada

--- src/silema/customersSilema.service.ts
+++ src/silema/customersSilema.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { getTokenService } from '../conection/getToken.service';
 import { runSqlService } from 'src/conection/sqlConection.service';
 import axios from 'axios';
+import { query } from 'express';
 
 @Injectable()
 export class customersSilemaService {
@@ -53,106 +54,20 @@ export class customersSilemaService {
       });
     for (let i = 0; i < res.data.value.length; i++) {
       if (res.data.value[i].processHIT) {
-        // console.log(`Cliente a procesar: ${res.data.value[i].number}`)
-        let sqlCodi = `SELECT MAX(t1.Codi + 1) AS codigo_disponible FROM clients t1 LEFT JOIN clients t2 ON t1.Codi + 1 = t2.Codi WHERE t2.Codi IS NULL;`
-        let queryCodi = await this.sql.runSql(sqlCodi, database)
+        let sqlNif = `select * from clients where Nif = '${res.data.value[i].vatRegistrationNo}'`
+        let queryNif = await this.sql.runSql(sqlNif, database)
+        if (queryNif.recordset.length == 0) {
+          // console.log(`Cliente a procesar: ${res.data.value[i].number}`)
+          let sqlCodi = `SELECT MAX(t1.Codi + 1) AS codigo_disponible FROM clients t1 LEFT JOIN clients t2 ON t1.Codi + 1 = t2.Codi WHERE t2.Codi IS NULL;`
+          let queryCodi = await this.sql.runSql(sqlCodi, database)
 
-        let Codi = queryCodi.recordset[0].codigo_disponible || 0; // Número convertido a entero o 0 si es inválido
-        let Nom = res.data.value[i].name || ""; // Nombre o cadena vacía
-        let Nif = res.data.value[i].vatRegistrationNo || ""; // CIF/NIF o cadena vacía
-        let Adresa = res.data.value[i].address || ""; // Dirección o cadena vacía
-        let Ciutat = res.data.value[i].city || ""; // Ciudad o cadena vacía
-        let Cp = res.data.value[i].postCode || 0; // Código postal como entero o 0
-        let NomLlarg = res.data.value[i].name || ""; // Nombre largo concatenado
-        let eMail = res.data.value[i].eMail || "";
-        let phone = res.data.value[i].phoneNo || "";
-        let FormaPago = res.data.value[i].paymentMethodCode || "";
-        let FormaPagoValor = 0;
-        let primaryContactNo = res.data.value[i].primaryContactNo || "";
-        let IdHitCFINAL = "";
-        let pagaEnTienda = res.data.value[i].payInStore || true;
-
-        let sqlSincroIds = `SELECT * FROM BC_SincroIds WHERE IdBc = '${res.data.value[i].number}'`;
-        let querySincro = await this.sql.runSql(sqlSincroIds, database)
-        if (querySincro.recordset.length == 0) {
-          // Insert clients
-          let sqlInsert = `INSERT INTO clients 
-        (Codi, Nom, Nif, Adresa, Ciutat, Cp, [Nom Llarg]) VALUES
-        (${Codi}, '${Nom}', '${Nif}', '${Adresa}', '${Ciutat}', '${Cp}', '${NomLlarg}')`
-          //console.log(sql)
-
-          let TipoDato = "customer"
-          let IdBc = res.data.value[i].number || "";
-          let IdHit = Codi;
-          let IdEmpresaBc = companyID;
-          let IdEmpresaHit = database;
-          sqlSincroIds = `INSERT INTO BC_SincroIds 
-          (TmSt, TipoDato, IdBc, IdHit, IdEmpresaBc, IdEmpresaHit) VALUES
-          (GETDATE(), '${TipoDato}', '${IdBc}', '${IdHit}', '${IdEmpresaBc}', '${IdEmpresaHit}')`
-
-          //Comprovar que paymentMethodCode es 
-          switch (FormaPago) {
-            case 'RebutDomiciliat':
-              FormaPagoValor = 1;
-              break;
-            case 'Cheque':
-              FormaPagoValor = 2;
-              break;
-            case 'CLI_EFECTIVO':
-              FormaPagoValor = 3;
-              break;
-            case 'CLI_TRANSF':
-              FormaPagoValor = 4;
-              break;
-          }
-          if (primaryContactNo != "") {
-            let sqlCFINAL = `SELECT * FROM BC_SincroIds WHERE IdBc = '${res.data.value[i].primaryContactNo}'`;
-            console.log(sqlCFINAL);
-            let queryCFINAL = await this.sql.runSql(sqlCFINAL, database)
-            if (queryCFINAL.recordset.length > 0 && queryCFINAL.recordset[0].IdHit != null) IdHitCFINAL = queryCFINAL.recordset[0].IdHit;
-            else console.log(`El "clientFinal" con IdBc *${res.data.value[i].primaryContactNo}* no existe en la base de datos o no tiene un IdHit`);
-          }
-
-          try {
-            let queryInsert = await this.sql.runSql(sqlInsert, database)
-            let queryInsertSincro = await this.sql.runSql(sqlSincroIds, database)
-            if (eMail != "")
-              await this.sqlConstantClient(Codi, 'eMail', eMail, 2, database)
-            if (phone != "")
-              await this.sqlConstantClient(Codi, 'Tel', phone, 2, database)
-            if (FormaPagoValor == 0)
-              await this.sqlConstantClient(Codi, 'FormaPagoLlista', FormaPagoValor, 2, database)
-            if (!pagaEnTienda)
-              await this.sqlConstantClient(Codi, 'NoPagaEnTienda', 'NoPagaEnTienda', 2, database)
-            if (IdHitCFINAL != "")
-              await this.sqlConstantClient(Codi, 'CFINAL', IdHitCFINAL, 2, database)
-            const data = {
-              processedHIT: true
-            };
-            let url2 = `${process.env.baseURL}/v2.0/${tenant}/${entorno}/api/abast/hitIntegration/v2.0/companies(${companyID})/customers(${res.data.value[i].id})`
-            const patchResponse = await axios.patch(url2, data, {
-              headers: {
-                Authorization: `Bearer ${token}`,
-                "Content-Type": "application/json",
-                "If-Match": "*",
-              },
-            });
-          } catch (error) {
-            throw new Error('Failed to put customer');
-          }
-          console.log("Customer procesado")
-        }
-        else {
-          Codi = querySincro.recordset[0].IdHit
-          let sqlUpdate = `UPDATE clients SET 
-          Nom = '${Nom}',
-          Nif = '${Nif}',
-          Adresa = '${Adresa}',
-          Ciutat = '${Ciutat}',
-          Cp = '${Cp}',
-          [Nom Llarg] = '${NomLlarg}'
-          WHERE Codi = ${Codi};`;
-          console.log(sqlUpdate);
+          let Codi = queryCodi.recordset[0].codigo_disponible || 0; // Número convertido a entero o 0 si es inválido
+          let Nom = res.data.value[i].name || ""; // Nombre o cadena vacía
+          let Nif = res.data.value[i].vatRegistrationNo || ""; // CIF/NIF o cadena vacía
+          let Adresa = res.data.value[i].address || ""; // Dirección o cadena vacía
+          let Ciutat = res.data.value[i].city || ""; // Ciudad o cadena vacía
+          let Cp = res.data.value[i].postCode || 0; // Código postal como entero o 0
+          let NomLlarg = res.data.value[i].name || ""; // Nombre largo concatenado
           let eMail = res.data.value[i].eMail || "";
           let phone = res.data.value[i].phoneNo || "";
           let FormaPago = res.data.value[i].paymentMethodCode || "";
@@ -160,53 +75,145 @@ export class customersSilemaService {
           let primaryContactNo = res.data.value[i].primaryContactNo || "";
           let IdHitCFINAL = "";
           let pagaEnTienda = res.data.value[i].payInStore || true;
-          switch (FormaPago) {
-            case 'RebutDomiciliat':
-              FormaPagoValor = 1;
-              break;
-            case 'Cheque':
-              FormaPagoValor = 2;
-              break;
-            case 'CLI_EFECTIVO':
-              FormaPagoValor = 3;
-              break;
-            case 'CLI_TRANSF':
-              FormaPagoValor = 4;
-              break;
-          }
-          if (primaryContactNo != "") {
-            IdHitCFINAL = querySincro.recordset[0].IdHit;
-          }
-          await this.sqlConstantClient(Codi, '', '', 5, database)
-          try {
-            let queryUpdate = await this.sql.runSql(sqlUpdate, database)
-            if (eMail != "")
-              await this.sqlConstantClient(Codi, 'eMail', eMail, 2, database)
-            if (phone != "")
-              await this.sqlConstantClient(Codi, 'Tel', phone, 2, database)
-            if (FormaPagoValor == 0)
-              await this.sqlConstantClient(Codi, 'FormaPagoLlista', FormaPagoValor, 2, database)
-            if (!pagaEnTienda)
-              await this.sqlConstantClient(Codi, 'NoPagaEnTienda', 'NoPagaEnTienda', 2, database)
-            if (IdHitCFINAL != "")
-              await this.sqlConstantClient(Codi, 'CFINAL', IdHitCFINAL, 2, database)
-            const data = {
-              processedHIT: true
-            };
-            let url2 = `${process.env.baseURL}/v2.0/${tenant}/${entorno}/api/abast/hitIntegration/v2.0/companies(${companyID})/customers(${res.data.value[i].id})`
-            const patchResponse = await axios.patch(url2, data, {
-              headers: {
-                Authorization: `Bearer ${token}`,
-                "Content-Type": "application/json",
-                "If-Match": "*",
-              },
-            });
-          } catch (error) {
-            throw new Error('Failed to put customer');
-          }
-          console.log("Customer actualizado")
-        }
 
+          let sqlSincroIds = `SELECT * FROM BC_SincroIds WHERE IdBc = '${res.data.value[i].number}'`;
+          let querySincro = await this.sql.runSql(sqlSincroIds, database)
+          if (querySincro.recordset.length == 0) {
+            // Insert clients
+            let sqlInsert = `INSERT INTO clients 
+          (Codi, Nom, Nif, Adresa, Ciutat, Cp, [Nom Llarg]) VALUES
+          (${Codi}, '${Nom}', '${Nif}', '${Adresa}', '${Ciutat}', '${Cp}', '${NomLlarg}')`
+            //console.log(sql)
+
+            let TipoDato = "customer"
+            let IdBc = res.data.value[i].number || "";
+            let IdHit = Codi;
+            let IdEmpresaBc = companyID;
+            let IdEmpresaHit = database;
+            sqlSincroIds = `INSERT INTO BC_SincroIds 
+          (TmSt, TipoDato, IdBc, IdHit, IdEmpresaBc, IdEmpresaHit) VALUES
+          (GETDATE(), '${TipoDato}', '${IdBc}', '${IdHit}', '${IdEmpresaBc}', '${IdEmpresaHit}')`
+
+            //Comprovar que paymentMethodCode es 
+            switch (FormaPago) {
+              case 'RebutDomiciliat':
+                FormaPagoValor = 1;
+                break;
+              case 'Cheque':
+                FormaPagoValor = 2;
+                break;
+              case 'CLI_EFECTIVO':
+                FormaPagoValor = 3;
+                break;
+              case 'CLI_TRANSF':
+                FormaPagoValor = 4;
+                break;
+            }
+            if (primaryContactNo != "") {
+              let sqlCFINAL = `SELECT * FROM BC_SincroIds WHERE IdBc = '${res.data.value[i].primaryContactNo}'`;
+              console.log(sqlCFINAL);
+              let queryCFINAL = await this.sql.runSql(sqlCFINAL, database)
+              if (queryCFINAL.recordset.length > 0 && queryCFINAL.recordset[0].IdHit != null) IdHitCFINAL = queryCFINAL.recordset[0].IdHit;
+              else console.log(`El "clientFinal" con IdBc *${res.data.value[i].primaryContactNo}* no existe en la base de datos o no tiene un IdHit`);
+            }
+
+            try {
+              let queryInsert = await this.sql.runSql(sqlInsert, database)
+              let queryInsertSincro = await this.sql.runSql(sqlSincroIds, database)
+              if (eMail != "")
+                await this.sqlConstantClient(Codi, 'eMail', eMail, 2, database)
+              if (phone != "")
+                await this.sqlConstantClient(Codi, 'Tel', phone, 2, database)
+              if (FormaPagoValor == 0)
+                await this.sqlConstantClient(Codi, 'FormaPagoLlista', FormaPagoValor, 2, database)
+              if (!pagaEnTienda)
+                await this.sqlConstantClient(Codi, 'NoPagaEnTienda', 'NoPagaEnTienda', 2, database)
+              if (IdHitCFINAL != "")
+                await this.sqlConstantClient(Codi, 'CFINAL', IdHitCFINAL, 2, database)
+              const data = {
+                processedHIT: true
+              };
+              let url2 = `${process.env.baseURL}/v2.0/${tenant}/${entorno}/api/abast/hitIntegration/v2.0/companies(${companyID})/customers(${res.data.value[i].id})`
+              const patchResponse = await axios.patch(url2, data, {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                  "Content-Type": "application/json",
+                  "If-Match": "*",
+                },
+              });
+            } catch (error) {
+              throw new Error('Failed to put customer');
+            }
+            console.log("Customer procesado")
+          }
+          else {
+            Codi = querySincro.recordset[0].IdHit
+            let sqlUpdate = `UPDATE clients SET 
+          Nom = '${Nom}',
+          Nif = '${Nif}',
+          Adresa = '${Adresa}',
+          Ciutat = '${Ciutat}',
+          Cp = '${Cp}',
+          [Nom Llarg] = '${NomLlarg}'
+          WHERE Codi = ${Codi};`;
+            console.log(sqlUpdate);
+            let eMail = res.data.value[i].eMail || "";
+            let phone = res.data.value[i].phoneNo || "";
+            let FormaPago = res.data.value[i].paymentMethodCode || "";
+            let FormaPagoValor = 0;
+            let primaryContactNo = res.data.value[i].primaryContactNo || "";
+            let IdHitCFINAL = "";
+            let pagaEnTienda = res.data.value[i].payInStore || true;
+            switch (FormaPago) {
+              case 'RebutDomiciliat':
+                FormaPagoValor = 1;
+                break;
+              case 'Cheque':
+                FormaPagoValor = 2;
+                break;
+              case 'CLI_EFECTIVO':
+                FormaPagoValor = 3;
+                break;
+              case 'CLI_TRANSF':
+                FormaPagoValor = 4;
+                break;
+            }
+            if (primaryContactNo != "") {
+              IdHitCFINAL = querySincro.recordset[0].IdHit;
+            }
+            await this.sqlConstantClient(Codi, '', '', 5, database)
+            try {
+              let queryUpdate = await this.sql.runSql(sqlUpdate, database)
+              if (eMail != "")
+                await this.sqlConstantClient(Codi, 'eMail', eMail, 2, database)
+              if (phone != "")
+                await this.sqlConstantClient(Codi, 'Tel', phone, 2, database)
+              if (FormaPagoValor == 0)
+                await this.sqlConstantClient(Codi, 'FormaPagoLlista', FormaPagoValor, 2, database)
+              if (!pagaEnTienda)
+                await this.sqlConstantClient(Codi, 'NoPagaEnTienda', 'NoPagaEnTienda', 2, database)
+              if (IdHitCFINAL != "")
+                await this.sqlConstantClient(Codi, 'CFINAL', IdHitCFINAL, 2, database)
+              const data = {
+                processedHIT: true
+              };
+              let url2 = `${process.env.baseURL}/v2.0/${tenant}/${entorno}/api/abast/hitIntegration/v2.0/companies(${companyID})/customers(${res.data.value[i].id})`
+              const patchResponse = await axios.patch(url2, data, {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                  "Content-Type": "application/json",
+                  "If-Match": "*",
+                },
+              });
+            } catch (error) {
+              throw new Error('Failed to put customer');
+            }
+            console.log("Customer actualizado")
+          }
+
+        } else {
+          console.log(`Customer con NIF *${res.data.value[i].vatRegistrationNo}* ya existe en la base de datos`);
+        }
       }
       console.log(`Synchronizing customers... -> ${i}/${res.data.value.length} --- ${((i / res.data.value.length) * 100).toFixed(2)}%`);
     }
