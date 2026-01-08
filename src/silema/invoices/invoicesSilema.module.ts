cc8eff3ba7a9a3973de672734d3228c6f7bc2b13
sheilamr <sheila.24.1996@gmail.com>
2026-01-08 08:23:22 +0100

    upd

--- src/silema/invoices/invoicesSilema.module.ts
+++ src/silema/invoices/invoicesSilema.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { intercompanySilemaController } from './intercompanySilema.controller';
 import { intercompanySilemaService } from './intercompanySilema.service';
-import { salesSilemaRecapController } from './salesSilemaRecap.controller';
-import { salesSilemaRecapService } from './salesSilemaRecap.service';
 import { salesSilemaRecapManualController } from './salesSilemaRecapManual.controller';
 import { salesSilemaRecapManualService } from './salesSilemaRecapManual.service';
 import { ConnectionModule } from 'src/connection/connection.module';
@@ -11,12 +9,10 @@ import { ConnectionModule } from 'src/connection/connection.module';
     imports: [ConnectionModule],
     controllers: [
         intercompanySilemaController,
-        salesSilemaRecapController,
         salesSilemaRecapManualController,
     ],
     providers: [
         intercompanySilemaService,
-        salesSilemaRecapService,
         salesSilemaRecapManualService,
     ],
 })
